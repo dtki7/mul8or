656518146f7414ed54c16d57140e276fba8afb3e
dtki7 <olideja@aol.com>
2016-06-21 14:38:44 +0200

    ?

--- emulate_file.cpp
+++ emulate_file.cpp
@@ -12,15 +12,15 @@ int main(int argc, const char* argv[]) {
   exe.seekg(0x429);
 
   int i = 0;
-  uint8_t buf[8] = { 0x06 };
+  uint8_t buf[8] = { 0x00 };
   cpu_x86_32 cpu;
   cpu.load_blank();
   while(true) {
     exe.read((char*)buf + i++, 1);
-    if(exe.tellg() > 0x463) {
+    if(exe.tellg() > 0x463 || i > 7) {
       return 0;
     }
-    if(cpu.load_change(buf, i) || i > 7) {
+    if(cpu.load_change(buf, i)) {
       i = 0;
       cpu.print();
       cin.get();
